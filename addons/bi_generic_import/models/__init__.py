b6eb17ce324fa4eb3a87d8f8f2c92f698d905b2e
Ernesto Garcia <ernesto.garcia@protektnet.com>
2018-02-21 13:02:09 -0600

    adding bi_generic_import

---
+++ addons/bi_generic_import/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from .import sale
+from .import account_invoice
+from . import purchase
+from .import stock
+from .import product
+from .import partner
+from .import picking
+from . import payment
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
